bbdaf6802203e618b9305074b8386f047b87b9a5
suny <suny@2345.com>
2016-08-24 11:50:14 +0800

    Improves Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
+
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token, :gravatar_url
 
@@ -98,6 +106,20 @@ class User < ActiveRecord::Base
   # 实现动态流原型
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # 关注另一个用户
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # 取消关注另一个用户
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
